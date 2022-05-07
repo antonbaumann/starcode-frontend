60a766d4f256ac5ebb75b5d552c221dbe454945c
Anton Baumann <anton@antonbaumann.com>
2022-05-07 20:17:54 +0200

    implement course section

--- src/components/home/FindCoursesSection.tsx
+++ src/components/home/FindCoursesSection.tsx
@@ -1,6 +1,22 @@
+import "./FindCoursesSection.scss";
+import girlsImage from "../../assets/img/4_girls.png"
+import GradientCircle from "../GradientCircle";
+import SectionTitle from "../SectionTitle"
+import Button from "../Button";
+
 const FindCoursesSection = () => {
     return (
-        <section className="find-courses">find courses</section>
+        <section className="find-courses">
+            <div className="GirlsImageOnCirlce">
+                <GradientCircle className="gradient-circle-home"/>
+                <img src={girlsImage} className="GirlsImage" />
+            </div>
+            <div className="content">
+                <SectionTitle className="section-title">Everyone<br/>Can Code</SectionTitle>
+                <p>Finde Kurse in deiner Nähe und<br /> starte deine Zukunft in der IT</p>
+                <Button text="Kurs finden"/>
+            </div>
+        </section>
     )
 }
 
