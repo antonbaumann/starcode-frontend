3207862abb3e944d039aae2ee46005d0ee649773
Anton Baumann <anton@antonbaumann.com>
2022-05-09 12:26:07 +0200

    implement whyStarcode section

--- src/components/home/WhyStarcodeSection.tsx
+++ src/components/home/WhyStarcodeSection.tsx
@@ -1,6 +1,22 @@
+import "./WhyStarcodeSection.scss";
+import SectionTitle from "../SectionTitle"
+import { ReactComponent as BlueStar } from '../../assets/svg/BlueStar.svg'
+import twoGirlsStar from '../../assets/img/2_girls_star.png'
+
 const WhyStarcodeSection = () => {
     return (
-        <section className="why-starcode">why starcode</section>
+        <section className="why-starcode">
+            <div className="content">
+                <SectionTitle className="section-title">Warum Starcode?</SectionTitle>
+                <p>
+                Wir bieten kostenlose Programmierkurse für Mädchen* zwischen 15 und 19 Jahren an, 
+                um ihnen die vielfältigen Möglichkeiten und Chancen der Informatik näher zu bringen 
+                und mit einer offenen und inspirierenden Community das Interesse an entsprechenden Studiengängen zu wecken. 
+                Gemeinsam wollen wir so für mehr Genderdiversität in der IT-Branche sorgen!
+                </p>
+            </div>
+            <img src={twoGirlsStar} className="GirlsImage" />
+        </section>
     )
 }
 
