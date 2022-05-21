ebffdf3f7cf47c1721c41f1f1434f570c675bedc
Anton Baumann <anton@antonbaumann.com>
2022-05-21 16:16:37 +0200

    implement lightMode footer

---
+++ src/components/TicktockIcon.tsx
@@ -0,0 +1,12 @@
+type TicktockIconProps = {
+    stroke?: string;
+    height?: string;
+}
+
+const TicktockIcon = ({stroke, height}: TicktockIconProps) => {
+    return (
+        <div></div>
+    )
+}
+
+export default TicktockIcon
