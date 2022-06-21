fdb116b90fc05bc272b6eeb88828dda4bbf81960
Anton Baumann <anton@antonbaumann.com>
2022-06-21 15:14:23 +0200

    strapi integration for team and faq

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';  
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from "@apollo/react-hooks";
+import {
+  ApolloClient,
+  InMemoryCache,
+} from '@apollo/client'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
+const client = new ApolloClient({
+  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+  cache: new InMemoryCache(),
+})
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App /> 
+    </ApolloProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
