8e76775f750656ab8070b7204247f28ea1dd8524
Anton Baumann <anton@antonbaumann.com>
2022-06-21 18:15:20 +0200

    use social media urls from strapi

---
+++ src/components/ExternalLink.tsx
@@ -0,0 +1,16 @@
+type ExternalLinkProps = {
+    to: string
+    target?: string
+    className?: string
+    children?: JSX.Element
+}
+
+const ExternalLink = ({to, target, className, children}: ExternalLinkProps) => {
+    return (
+        <a href={to} target={target} rel="noopener noreferrer" className={className}>
+            {children}
+        </a>
+    )
+}
+
+export default ExternalLink
