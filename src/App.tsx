500c66aaec776e8b374c227db5157f797402440b
Anton Baumann <anton@antonbaumann.com>
2022-06-23 11:18:45 +0200

    google analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Page from './components/Page';
 import AboutView from './views/AboutView';
 import CoursesView from './views/CoursesView';
 import HomeView from './views/HomeView'
@@ -16,15 +17,15 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/mission" element={<MissionView />} />
-          <Route path="/courses" element={<CoursesView />} />
-          <Route path="/about" element={<AboutView />} />
-          <Route path="/volunteer" element={<VolunteerView />} />
-          <Route path="/donate" element={<DonateView />} />
-          <Route path="/contact" element={<ContactView />} />
-          <Route path='/faq' element={<QuestionAnswerView />} />
-          <Route path='/imprint' element={<ImprintView />} />
+          <Route path="/" element={<Page title='Starcode'><HomeView /></Page>} />
+          <Route path="/mission" element={<Page title='Starcode | Mission'><MissionView /></Page>} />
+          <Route path="/courses" element={<Page title='Starcode | Kurse'><CoursesView /></Page>} />
+          <Route path="/about" element={<Page title='Starcode | Über uns'><AboutView /></Page>} />
+          <Route path="/volunteer" element={<Page title='Starcode | Volunteer'><VolunteerView /></Page>} />
+          <Route path="/donate" element={<Page title='Starcode | Spenden'><DonateView /></Page>} />
+          <Route path="/contact" element={<Page title='Starcode | Kontakt'><ContactView /></Page>} />
+          <Route path='/faq' element={<Page title='Starcode | FAQ'><QuestionAnswerView /></Page>} />
+          <Route path='/imprint' element={<Page title='Starcode | Impressum'><ImprintView /></Page>} />
         </Routes>
       </Router>
     </div>
