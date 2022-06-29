a34568d7e3c1906ce8afb9ff4a8b7ca3dda3f7dd
Anton Baumann <anton@antonbaumann.com>
2022-06-29 12:52:04 +0200

    add content to volunteer groups

--- src/components/volunteer/GroupsSection.tsx
+++ src/components/volunteer/GroupsSection.tsx
@@ -3,6 +3,8 @@ import SectionTitle from '../SectionTitle'
 import { VolunteerGroup, volunteerGroups } from '../../data/volunteerGroups'
 import { useState } from 'react'
 import classNames from 'classnames'
+import ExternalLink from '../ExternalLink'
+import { sendEvent } from '../../util/analytics'
 
 const GroupsSection = () => {
   const [selectedGroup, setSelectedGroup] = useState<VolunteerGroup>(
@@ -11,7 +13,10 @@ const GroupsSection = () => {
 
   const handleClick = (groupTitle: string) => {
     const group = volunteerGroups.find((group) => group.title === groupTitle)
-    if (group) setSelectedGroup(group)
+    if (group) {
+      setSelectedGroup(group)
+      sendEvent('volunteer_group_info', { group: groupTitle })
+    }
   }
 
   return (
@@ -33,7 +38,12 @@ const GroupsSection = () => {
         </div>
         <div className="content">
           <h3>{selectedGroup.title}</h3>
-          <p>{selectedGroup.description}</p>
+          <p>
+            {selectedGroup.description + ' '}
+            <ExternalLink to={'mailto:' + selectedGroup.contactEmail}>
+              <>{selectedGroup.contactEmail}</>
+            </ExternalLink>
+          </p>
         </div>
       </div>
     </section>
