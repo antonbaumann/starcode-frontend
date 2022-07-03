8dfefa1fc01128fad84b9158a1eebb156442c850
Anton Baumann <30725692+antonbaumann@users.noreply.github.com>
2022-07-03 23:23:15 +0200

    add imprint text (#55)

--- src/views/ImprintView.tsx
+++ src/views/ImprintView.tsx
@@ -1,3 +1,4 @@
+import ExternalLink from '../components/ExternalLink'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import TextSection from '../components/imprint/TextSection'
@@ -6,14 +7,218 @@ const ImprintView = () => {
   return (
     <main className="imprint">
       <Header />
-      <TextSection
-        title="Imprint"
-        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-      />
-      <TextSection
-        title="Privacy"
-        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-      />
+      <TextSection title="Impressum">
+        <>
+          <b>Angaben gemäß § 5 TM</b>
+          <br />
+          <br />
+          StarCode e.V.
+          <br />
+          Heilmannstraße 15
+          <br />
+          81479 München
+          <br />
+          Vereinsregister München, Nr. 209442
+          <br />
+          <br />
+          Vertreten durch:
+          <br />
+          Die jeweilige 1. Vorsitzende, 2. Vorsitzende und Vorständin für
+          Finanzen.
+          <br />
+          Im Moment Julia Moncayo von Hase, Friedrich Wicke und Caroline
+          Kloepfer.
+          <br />
+          <br />
+          <br />
+          <b>Kontakt</b>
+          <br />
+          <br />
+          Telefon: +49 152 29676216
+          <br />
+          Email: friedrich.wicke@protonmail.com
+          <br />
+          <br />
+          <br />
+          <b>Konzeption und Entwicklung</b>
+          <br />
+          <br />
+          Programmierung:{' '}
+          <ExternalLink to="https://antonbaumann.com">
+            <>Anton Baumann</>
+          </ExternalLink>
+          <br />
+          <br />
+          <br />
+          <b>Verbraucherstreitbeilegung/Universalschlichtungsstelle</b>
+          <br />
+          Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+          vor einer Verbraucherschlichtungsstelle teilzunehmen.
+          <br />
+          <br />
+          <br />
+          <b>Haftung für Inhalte</b>
+          <br />
+          <br />
+          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
+          auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+          §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+          verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+          überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+          Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
+          Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
+          hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+          Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei
+          Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+          Inhalte umgehend entfernen.
+          <br />
+          <br />
+          <br />
+          <b>Haftung für Links</b>
+          <br />
+          <br />
+          Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
+          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+          <br />
+          Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
+          ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
+          <br />
+          <br />
+          <br />
+          <b>Urheberrecht</b>
+          <br />
+          <br />
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+          Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+          Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+          Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+          sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+          <br />
+          <br />
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
+          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
+          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+          werden wir derartige Inhalte umgehend entfernen.
+        </>
+      </TextSection>
+
+      <TextSection title="Privacy">
+        <>
+          <b>I. Pflichtangaben</b>
+          <br />
+          1. Name und Kontaktdaten des Verantwortlichen
+          <br />
+          StarCode e.V.
+          <br />
+          Heilmannstraße 15
+          <br />
+          81479 München
+          <br />
+          <br />
+          2. Kontaktdaten des Datenschutzkoordinators <br />
+          Der Vorstand für Rechtliches <br />
+          leon.reitz@outlook.de <br />
+          <br />
+          3. Zwecke und Rechtsgrundlagen der Verarbeitung <br />
+          Ihre Daten werden dafür erhoben, um die Organisation und Durchführung
+          von Veranstaltungen von StarCode e.V. zu organisieren. <br />
+          Ihre Daten werden auf Grundlage von Art. 6 Abs. 1 lit. b DSGVO
+          verarbeitet. <br />
+          <br />
+          4. Kategorien der personenbezogenen Daten, die verarbeitet werden
+          <br />
+          StarCode e.V. verarbeitet folgende Kategorien von personenbezogenen
+          Daten
+          <br />
+          von Ihnen:
+          <br />
+          - Allgemeine Persönlichkeitsdaten
+          <br />
+          - Kontaktdaten
+          <br />
+          <br />
+          5. Empfänger oder Kategorien von Empfängern der personenbezogenen
+          Daten Ihre personenbezogenen Daten werden außerdem übermittelt an:
+          <br />
+          - Google (soweit Google die Daten über die Verwendung dieses Dienstes
+          festhält)
+          <br />
+          <br />
+          6. Übermittlung von personenbezogenen Daten an ein Drittland
+          <br />
+          Die Bereitstellung Ihrer Daten erfolgt über eine Webanwendung im
+          Internet. Die Vertraulichkeit, die Integrität (Unverletzlichkeit), die
+          Authentizität (Echtheit) und die Verfügbarkeit der personenbezogenen
+          Daten kann damit nicht garantiert werden. Ihre Daten können über die
+          Webanwendung auch in Drittländern abgerufen werden, die keine der
+          Bundesrepublik Deutschland vergleichbaren Datenschutzbestimmungen
+          kennen, insbesondere in den USA (Google-Server).
+          <br />
+          <br />
+          <br />
+          <b>II. Weitere allgemeine Pflichtangaben</b>
+          <br />
+          - Ihre Daten werden nach der Erhebung und Ende der Veranstaltung
+          umgehend gelöscht.
+          <br />
+          <br />
+          - Betroffenenrechte
+          <br />
+          Nach der EU-Datenschutzgrundverordnung stehen Ihnen folgende Rechte
+          zu:
+          <br />
+          Werden Ihre personenbezogenen Daten verarbeitet, so haben Sie das
+          Recht Auskunft über die zu Ihrer Person gespeicherten Daten zu
+          erhalten (Art. 15 DSGVO).
+          <br />
+          Sollten unrichtige personenbezogene Daten verarbeitet werden, steht
+          Ihnen ein Recht auf Berichtigung zu (Art. 16 DSGVO).
+          <br />
+          Liegen die gesetzlichen Voraussetzungen vor, so können Sie die
+          Löschung oder Einschränkung der Verarbeitung verlangen sowie
+          Widerspruch gegen die Verarbeitung einlegen (Art. 17, 18 und 21
+          DSGVO).
+          <br />
+          <br />
+          Zur Ausübung Ihrer Rechte wenden Sie sich bitte an den oben
+          bezeichneten Datenschutzkoordinator. Bei datenschutzrechtlichen
+          Beschwerden können Sie sich an die zuständige Aufsichtsbehörde wenden:
+          <br />
+          <br />
+          Bayerisches Landesamt für Datenschutzaufsicht
+          <br />
+          Promenade 27 (Schloss)
+          <br />
+          91522 Ansbach
+          <br />
+          Telefon: +49 (0) 981 53 1300
+          <br />
+          Telefax: +49 (0) 981 53 98 1300
+          <br />
+          E-Mail: poststelle@lda.bayern.de
+          <br />
+          <br />
+          - Widerrufsrecht bei Einwilligung
+          <br />
+          Wenn Sie in die Verarbeitung durch StarCode e.V. eine entsprechende
+          Erklärung eingewilligt haben, können Sie die Einwilligung jederzeit
+          für die Zukunft widerrufen.
+          <br />
+          Die Rechtmäßigkeit der aufgrund der Einwilligung bis zum Widerruf
+          erfolgten Datenverarbeitung wird durch diesen nicht berührt.
+          <br />
+        </>
+      </TextSection>
       <Footer />
     </main>
   )
