3b0fdfb168d6847b6920cc7cd2c057a058872996
Anton Baumann <30725692+antonbaumann@users.noreply.github.com>
2022-07-08 17:36:13 +0200

    content fixes (#62)

--- src/components/home/WhyStarcodeSection.tsx
+++ src/components/home/WhyStarcodeSection.tsx
@@ -8,12 +8,12 @@ const WhyStarcodeSection = () => {
       <div className="content">
         <SectionTitle className="section-title">Warum Starcode?</SectionTitle>
         <p>
-          Wir bieten kostenlose Programmierkurse für Mädchen* zwischen 15 und 19
-          Jahren an, um ihnen die vielfältigen Möglichkeiten und Chancen der
-          Informatik näher zu bringen und mit einer offenen und inspirierenden
-          Community das Interesse an entsprechenden Studiengängen zu wecken.
-          Gemeinsam wollen wir so für mehr Genderdiversität in der IT-Branche
-          sorgen!
+          Wir bieten als gemeinnütziger Verein kostenlose Programmierkurse für
+          Mädchen* zwischen 11 und 19 Jahren an, um ihnen die vielfältigen
+          Möglichkeiten und Chancen der Informatik näher zu bringen und mit
+          einer offenen und inspirierenden Community das Interesse an
+          entsprechenden Studiengängen zu wecken. Gemeinsam wollen wir so für
+          mehr Genderdiversität in der IT-Branche sorgen!
         </p>
       </div>
       <img src={twoGirlsStar} className="GirlsImage" alt="two girls" />
